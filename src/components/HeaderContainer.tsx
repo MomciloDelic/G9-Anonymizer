7423ed5947a50a8882449d49de9f8d47138a408d
Thevy Ngo <thevyngo.1998@gmail.com>
2020-02-09 22:11:51 +0100

    Add content

--- src/components/HeaderContainer.tsx
+++ src/components/HeaderContainer.tsx
@@ -6,7 +6,7 @@ import Container from "@material-ui/core/Container";
 const useStyles = makeStyles(theme => ({
   heroContent: {
     minHeight: "70vh"
-  },
+  }
 }));
 
 const HeaderContainer: React.FC = () => {
@@ -14,33 +14,37 @@ const HeaderContainer: React.FC = () => {
 
   return (
     <React.Fragment>
-      <Container maxWidth='xl' component='main' className={classes.heroContent}>
-        <div style={{ padding: "5%", marginTop: "8%"}}>
-          <Typography
-            style={{ color: "black"}}
-            component='h3'
-            variant='h3'
-            align='left'
-            color='textPrimary'
-            gutterBottom
-          >
-            <span style={{ color: "#ffa834" }}>Anonymizer </span> turns domain <br/>knowledge into code
-          </Typography>
+      <div
+        style={{ padding: "5%", marginTop: "8%" }}
+        className={classes.heroContent}
+      >
+        <Typography
+          style={{ color: "black" }}
+          component='h3'
+          variant='h3'
+          align='left'
+          color='textPrimary'
+          gutterBottom
+        >
+          <span style={{ color: "#ffa834" }}>Anonymizer </span> turns domain
           <br />
-          <br />
-{/*           <Typography
-            variant='h5'
-            align='center'
-            color='textSecondary'
-            component='p'
-          >
-            Lorem Ipsum has been the industry's standard dummy text ever since
-            the 1500s, when an unknown printer. Lorem Ipsum has been the
-            industry's standard.
-          </Typography> */}
-          <hr style={{ width: "30%", float: "left"}} />
-        </div>
-      </Container>
+          knowledge into code
+        </Typography>
+
+        <hr style={{ width: "30%", float: "left" }} />
+        <br />
+        <Typography
+          variant='h5'
+          color='textSecondary'
+          component='p'
+          style={{ marginTop: "2rem", width: "60%" }}
+        >
+          Innføring av GDPR har store konsekvenser for behandling av personlig
+          og sensitiv data. Anonymizer gir støtte for enkelt å lage programmer
+          som understøtter funksjonelle krav som GDPR pålegger og g9 Anonymizer
+          kan produsere databaser anonymisert for test- og utviklingspersonell
+        </Typography>
+      </div>
     </React.Fragment>
   );
 };
