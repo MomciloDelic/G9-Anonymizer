3b5833fe2c1cabd011c06d12babc78c56ebc56f2
Thevy Ngo <thevyngo.1998@gmail.com>
2020-02-10 17:35:33 +0100

    Project structure

---
+++ src/pages/Layout.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import CardContainer from "../components/CardContainer";
+import HeaderContainer from "../components/HeaderContainer";
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
+import OmOss from "../pages/OmOss";
+
+const Layout: React.FC = () => {
+  return (
+    <React.Fragment>
+      <Navbar />
+
+      <HeaderContainer />
+      <CardContainer />
+      <Footer />
+    </React.Fragment>
+  );
+};
+
+export default Layout;
---
+++ src/pages/OmOss.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Container from "@material-ui/core/Container";
+import Typography from "@material-ui/core/Typography";
+
+const OmOss = () => {
+  return (
+    <Container>
+      <Typography variant='h2'>Om oss</Typography>
+      <Typography variant='body1'>
+        Esito utvikler programvare i nisjemarkedet “Low code development tools”
+        og tilbyr konsulenttjenester innen Java utvikling. Esito er et selskap
+        med høy komptanse og lang erfaring innen produkt- og systemutvikling.
+        Sammen med konsulenter fra Inmeta Consulting, Crayon og andre
+        samarbeidspartnere inngår våre konsulenter i team som ivaretar kundens
+        behov innen digitalisering og systemutvikling. Esito er datterselskap av
+        Inmeta Consulting og en del av Crayon group.
+      </Typography>
+    </Container>
+  );
+};
+
+export default OmOss;
