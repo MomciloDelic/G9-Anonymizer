d71356384ff97f8b15c27c045f7137359cc5c24e
Thevy Ngo <thevyngo.1998@gmail.com>
2020-02-12 19:20:24 +0100

    Update App.tsx

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,17 @@
 import React from "react";
+import { Route, Router, Switch } from "react-router-dom";
 import {
   createMuiTheme,
   MuiThemeProvider,
   responsiveFontSizes
 } from "@material-ui/core/styles";
-import { Route, Router, Switch } from "react-router-dom";
+import CssBaseline from "@material-ui/core/CssBaseline";
+
 import history from "../history";
+import Navbar from "./Navbar";
 import Home from "../pages/Home";
+import Kontakt from "../pages/Kontakt";
+import Footer from "../components/Footer";
 
 let theme = createMuiTheme({
   typography: {
@@ -20,9 +25,14 @@ const App = () => {
   return (
     <MuiThemeProvider theme={theme}>
       <Router history={history}>
+        <Navbar />
+        <CssBaseline />
+
         <Switch>
           <Route path='/' exact component={Home} />
+          <Route path='/kontakt' exact component={Kontakt} />
         </Switch>
+        <Footer />
       </Router>
     </MuiThemeProvider>
   );
